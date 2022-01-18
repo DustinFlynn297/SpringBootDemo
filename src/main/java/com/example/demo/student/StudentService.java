fa041474a3b966f57b6957c616a1020490344339
Dustin Flynn <flynnd297@gmail.com>
2022-01-17 20:31:27 -0600

    Added delete functionality to our student controller and student service.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -25,5 +25,15 @@ public class StudentService {
         if (studentOptional.isPresent()) {
             throw new IllegalStateException("email taken");
         }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+            throw new IllegalStateException(
+                    "student with id " + studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
     }
 }
